08eed71d89b58e326610b9804d8085e84cd58e51
Shreyas0988 <145433465+Shreyas0988@users.noreply.github.com>
2025-01-17 00:52:02 +0800

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,8 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      height: 700,
-    });
-    calendar.render();
+import { getUid } from "./firebase.js";
+import { addEvents } from "./firestore.js";
+
+let calendar;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar');
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    height: 700,
+    dateClick: function(info) {
+      const title = prompt('Enter Event Title:');
+      if (title) {
+        calendar.addEvent({
+          title: title,
+          start: info.dateStr,
+        });
+        getUid()
+        .then(uid => {
+          addEvents(uid, title, info.dateStr);
+          const taskList = document.getElementById("event-list");
+          const newTask = document.createElement('li');
+          newTask.textContent = `${title}`;
+          taskList.appendChild(newTask);
+
+            
+        })
+        .catch(error => {
+            console.log(error);  
+        });
+      }
+    },
   });
+  calendar.render();
+});
+
+export function addEventToCalendar(title, date) {
+  if (calendar) {
+    console.log("Calendar function working");
+    calendar.addEvent({ title: title, start: date});
+  } else {
+    console.error("Calendar is not initialized");
+  }
+}
